57de4bc04818619cc46e3351342078eb58075914
Ahsanul Hoque <hoque.talha@gmail.com>
2021-06-17 19:58:57 +0600

    17th june commit

---
+++ project codebase/testimonial.php
@@ -0,0 +1,110 @@
+<div class="reviews">
+    <div class="container" id="reviews">
+        <div class="heading animate-box bordery">
+            <h2><b>CUSTOMER LOVE US</b></h2>
+        </div>
+        <div class="text-center animate-box bordery">
+            <h3>We serve them well</h3>
+        </div>
+        <div class="row animate-box bordery">
+            <div class="col-md-12">
+                <div id="quote-carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active item">
+                            <blockquote>
+                                <div class="row">
+                                    <div class="col-sm-8 offset-sm-2">
+                                        <p class="testi">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                            veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
+                                        <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                        <div class="text-center">
+                                            <h4 class="text-org">RUBITRUX</h4>
+                                            <h5 class="text-dark">Boone, North Carolina, USA</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item item">
+                            <blockquote>
+                                <div class="row">
+                                    <div class="col-sm-8 offset-sm-2">
+                                        <p class="testi">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex </p>
+                                        <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                        <div class="text-center">
+                                            <h4 class="text-org">ACCUSTANDARD</h4>
+                                            <h5 class="text-dark">UI Designer</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </blockquote>
+                        </div>
+                        <div class="carousel-item item">
+                            <blockquote>
+                                <div class="row">
+                                    <div class="col-sm-8 offset-sm-2">
+                                        <p class="testi">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                            veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
+                                        <div class="text-center"><i class="fa fa-caret-down down-ar"></i></div>
+                                        <div class="text-center">
+                                            <h4 class="text-org">BOAT LOCKER</h4>
+                                            <h5 class="text-dark">UI Designer</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </blockquote>
+                        </div>
+                    </div>
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#quote-carousel" data-slide-to="0" class="active"><img class="img-responsive " src="img/Rubitrux.jpg" alt="">
+                        </li>
+                        <li data-target="#quote-carousel" data-slide-to="1"><img class="img-responsive" src="https://d2yy1nggxann9w.cloudfront.net/wp-content/uploads/2017/02/accustandard-1024x1024.jpg" alt="accustandard">
+                        </li>
+                        <li data-target="#quote-carousel" data-slide-to="2"><img class="img-responsive" src="https://d2yy1nggxann9w.cloudfront.net/wp-content/uploads/2018/08/BoatLockerFeatImg-1024x1024.jpg" alt="">
+                        </li>
+                    </ol>
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="testimonial">
+
+
+    <div class="slideshow-container">
+
+        <div class="mySlides">
+            <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
+            <p class="author">- John Keats</p>
+        </div>
+
+        <div class="mySlides">
+            <q>But man is not made for defeat. A man can be destroyed but not defeated.</q>
+            <p class="author">- Ernest Hemingway</p>
+        </div>
+
+        <div class="mySlides">
+            <q>I have not failed. I've just found 10,000 ways that won't work.</q>
+            <p class="author">- Thomas A. Edison</p>
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+
+    </div>
+
+    <div class="dot-container">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+</div>
